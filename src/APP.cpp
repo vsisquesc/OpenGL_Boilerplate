69998f5517dedd6ee4a4de6f33ad518b07d8bb58
vsisquesc <vsisques@gmail.com>
2024-05-30 21:32:08 +0200

    boilerplate acabado, falta función _init en la clase APP, usar framebuffer y  documentar/comentar todo

--- src/APP.cpp
+++ src/APP.cpp
@@ -80,36 +80,7 @@ void APP::_resetValues() {
 
 void APP::_dockSpace() {
 
-    static ImGuiDockNodeFlags dockspace_flags = ImGuiDockNodeFlags_None;
-
-    // We are using the ImGuiWindowFlags_NoDocking flag to make the parent window not dockable into,
-    // because it would be confusing to have two docking targets within each others.
-    ImGuiWindowFlags window_flags = ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDocking;
-
-    dockspace_flags &= ~ImGuiDockNodeFlags_PassthruCentralNode;
-    const ImGuiViewport *viewport = ImGui::GetMainViewport();
-    ImGui::SetNextWindowPos(viewport->WorkPos);
-    ImGui::SetNextWindowSize(viewport->WorkSize);
-    ImGui::SetNextWindowViewport(viewport->ID);
-    ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
-    ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
-    window_flags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
-    window_flags |= ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
-    if (dockspace_flags & ImGuiDockNodeFlags_PassthruCentralNode)
-        window_flags |= ImGuiWindowFlags_NoBackground;
-
-    ImGui::Begin("DockSpace", nullptr, window_flags);
-
-    ImGui::PopStyleVar(2);
-
-    // Submit the DockSpace
-    ImGuiIO &io = ImGui::GetIO();
-    if (io.ConfigFlags & ImGuiConfigFlags_DockingEnable) {
-        ImGuiID dockspace_id = ImGui::GetID("MyDockSpace");
-        ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), dockspace_flags);
-    }
-
-    ImGui::End();
+    ImGui::DockSpaceOverViewport(0, ImGui::GetMainViewport());
 }
 
 static void glfw_error_callback(int error, const char *description) {
